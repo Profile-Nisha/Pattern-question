546ed922cbc66d7df06acf804e9b5cd30d077979
nisha kumari <nishakumari7664@gmail.com>
2024-02-26 12:27:33 +0530

    my commit

---
+++ Character.java
@@ -0,0 +1,14 @@
+public class Character {
+    public static void main(String args[]){
+        int n =5;
+        char ch= 'A';
+        for(int i = 0; i<=n; i++){
+            for(int chars = 1; chars<=i;chars++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
